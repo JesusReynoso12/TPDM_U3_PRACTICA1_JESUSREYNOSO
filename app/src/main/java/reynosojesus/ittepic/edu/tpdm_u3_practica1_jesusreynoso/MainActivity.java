4be8a07483fc7b56e2721dbf73e1c8ff2780c504
JesusReynoso12 <jealreynosome@ittepic.edu.mx>
2019-04-22 23:09:32 -0600

    sadasd

--- app/src/main/java/reynosojesus/ittepic/edu/tpdm_u3_practica1_jesusreynoso/MainActivity.java
+++ app/src/main/java/reynosojesus/ittepic/edu/tpdm_u3_practica1_jesusreynoso/MainActivity.java
@@ -2,8 +2,14 @@ package reynosojesus.ittepic.edu.tpdm_u3_practica1_jesusreynoso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText nombreP,razaP,edadP,descripcionP;
+    Button insertarP,eliminarP,actualizarP,consultarP;
+    ListView listaP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
